12c753b14a15736419bc516a96f73eb41ec644d5
André Borelli <andreborelli729@gmail.com>
2024-11-22 17:51:07 -0300

    new updated
    page the ranch done. Site is not done yet.

---
+++ src/components/RanchWaveMobile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/ranchWaveMobile.css';
+import ranchWave from '../assets/ranchWave.jpg';
+
+function RanchWaveMobile(){
+    return(
+        <section className="ranchWaveMobile">
+          <img src={ranchWave} alt="inside a wave tube image" className="ranchWaveImgMobile"></img>
+        <h2 className="ranchWaveTitleMobile">The Wave</h2>
+        <p className="ranchWaveParagraphMobile">The central feature of Surf Ranch is the wave, the first of its kind. We built a 6-foot barreling wave that travels over 2300 feet yielding up to minute long rides. The wave can be controlled allowing a wave riding experience for surfers of all skill levels.</p>
+        
+        </section>
+    );
+
+}
+
+
+export default RanchWaveMobile;
--- src/pages/Ranch.js
+++ src/pages/Ranch.js
@@ -4,14 +4,26 @@ import RanchBanner from '../components/RanchBanner.js';
 import RanchFooter from '../components/RanchFooter.js';
 import RanchServiceMobile from '../components/RanchServiceMobile.js';
 import RanchService from '../components/RanchService.js';
+import RanchWaveMobile from '../components/RanchWaveMobile.js';
+import RanchWave from '../components/RanchWave.js';
+import RanchExperienceMobile from '../components/RanchExperienceMobile.js';
+import RanchExperience from '../components/RanchExperience.js';
+import RanchWslMobile from '../components/RanchWslMobile.js';
+import RanchWsl from '../components/RanchWsl.js';
 
 function Ranch(){
     return(
         <>
         <RanchBannerMobile/>
         <RanchBanner/>  
+        <RanchWaveMobile/>
+        <RanchWave/>
+        <RanchExperienceMobile/>
+        <RanchExperience/>
         <RanchServiceMobile/>
         <RanchService/>
+        <RanchWslMobile/>
+        <RanchWsl/>
         <RanchFooter/>
         </>
     );
